a7f63c8777f09d94dfad7cb355b1fe2a9bb89ed2
waghtot <waghtot@gmail.com>
2020-05-25 19:24:52 +0100

    menu update

--- app/views/template/template.php
+++ app/views/template/template.php
@@ -17,7 +17,7 @@
   </head>
   <body>
 
-  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow sticky-top">
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white sticky-top">
       <h5 class="my-0 mr-md-auto font-weight-normal" id="nav-home">RedBrick.com</h5>
       <nav class="my-2 my-md-0 mr-md-3">
       <?php
